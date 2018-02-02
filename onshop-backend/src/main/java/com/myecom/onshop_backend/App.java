b1dacead79b89a412ee3101fb12af79813456495
ram <sandhyasharma0423@gmail.com>
2018-02-02 15:39:43 +0530

    my project started stage

---
+++ onshop-backend/src/main/java/com/myecom/onshop_backend/App.java
@@ -0,0 +1,13 @@
+package com.myecom.onshop_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
